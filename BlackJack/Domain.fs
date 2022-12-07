22c85bee7db02fd0b155d71a411c366f1a3a1902
andradefr8 <85421753+andradefr8@users.noreply.github.com>
2022-12-07 15:27:42 -0300

    Add project files.

---
+++ BlackJack/Domain.fs
@@ -0,0 +1,26 @@
+﻿module Domain
+
+type Suit = 
+    | Heart
+    | Spade
+    | Club
+    | Diamond
+
+type Face = 
+    | Ace
+    | King
+    | Queen
+    | Jack
+    | Number of int
+
+type Card = {
+    Face : Face
+    Suit : Suit
+}
+
+type Money = Money of int
+type Bet = Bet of Money
+type Pot = Pot of Money
+
+type Deck = Card list
+type Hand = Card list
